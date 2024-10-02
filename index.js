5bce2d7cc44af5296b31e56306f5c4dcb8852fea
Ameya Gurjar <amey992004@gmail.com>
2024-10-02 19:53:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-meow
+const Discord = require("discord.js")
+require("dotenv").config();
+const client = new Discord.Client()
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+})
+
+client.login(process.env.TOKEN)
