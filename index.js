0fc53ec8d4bc082118131469fd139e5dfff89480
Ameya Gurjar <amey992004@gmail.com>
2024-10-02 22:34:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const Canvas = require("canvas");
-const mainCanvas = Canvas.createCanvas(1920, 1080);
+const mainCanvas = Canvas.createCanvas(1280, 720);
 const MainCanvas = mainCanvas.getContext("2d");
 require("dotenv").config();
 const client = new Discord.Client({intents:Object.keys(Discord.GatewayIntentBits)});
@@ -12,7 +12,7 @@ client.on("ready", () => {
 client.on(Discord.Events.MessageCreate, async(msg)=>{
   if (!msg.author.bot) {
     MainCanvas.fillStyle = "rgba(100,100,100,255)";
-    MainCanvas.fillRect(0,0,1920,1080);
+    MainCanvas.fillRect(0,0,mainCanvas.width,mainCanvas.height);
     //console.log(msg.member.displayAvatarURL({format:"png"}));
     const UserAvatar = await Canvas.loadImage("https://raw.githubusercontent.com/ZukaBots/ZukaAssets/main/CatWelcome.png").then(img=>{
       MainCanvas.drawImage(img,700,700,500,500);
