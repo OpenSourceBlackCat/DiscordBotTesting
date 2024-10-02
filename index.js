4907cf7cc0c6fec8dc271c0f7e538c9f183e2bf6
Ameya Gurjar <amey992004@gmail.com>
2024-10-02 20:27:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 const Canvas = require("canvas");
+const mainCanvas = Canvas.createCanvas(1920, 1080);
+const MainCanvas.getContext("2d");
 require("dotenv").config();
 const client = new Discord.Client({intents:Object.keys(Discord.GatewayIntentBits)});
 
@@ -9,7 +11,16 @@ client.on("ready", () => {
 
 client.on(Discord.Events.MessageCreate, async(msg)=>{
   if (!msg.author.bot) {
-    await msg.channel.send("pong");
+    MainCanvas.fillStyle("rgba(0,0,0,0)");
+    MainCanvas.fillRect(0,0,1920,1080);
+    const UserAvatar = new Image();
+    UserAvatar.src = msg.user.avatarURL();
+    UserAvatar.onload = async() => {
+      MainCanvas.drawImage(UserAvatar,700,700,500,500);
+    };
+    const buffer = mainCanvas.toBuffer("image/png");
+    const meow = new Discord.AttachmentBuilder(buffer, {name:"meow.png"});
+    await msg.channel.send({content:"Meow", files:[meow]});
   }
 })
 
