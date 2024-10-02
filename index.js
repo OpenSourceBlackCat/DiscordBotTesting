a5997f81672f74747ed641dfd315d26212668bb4
Ameya Gurjar <amey992004@gmail.com>
2024-10-02 20:01:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ client.on("ready", () => {
 })
 
 client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
+  if (true) {
+    await msg.channel.send("pong");
   }
 })
 
