8a941795e12976a575388c254497920d5d1a779c
Ameya Gurjar <amey992004@gmail.com>
2024-10-02 20:35:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.on(Discord.Events.MessageCreate, async(msg)=>{
     MainCanvas.fillStyle = "rgba(0,0,0,0)";
     MainCanvas.fillRect(0,0,1920,1080);
     const UserAvatar = new Canvas.Image();
-    UserAvatar.src = msg.author.avatarURL();
+    UserAvatar.src = msg.author.displayAvatarURL;
     console.log(UserAvatar.src);
     UserAvatar.onload = async() => {
       MainCanvas.drawImage(UserAvatar,700,700,500,500);
